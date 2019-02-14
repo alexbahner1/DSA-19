ff9bd017fa486cd7a9915a2657629f2950aec6a7
Alex Bahner <abahner1@gmail.com>
2019-02-13 22:08:14 -0500

    Merge branch 'master' of https://github.com/Olin-DSA/DSA-19

--- datastructures/day03/src/Problems.java
+++ datastructures/day03/src/Problems.java
@@ -15,19 +15,85 @@ public class Problems {
     public static List<Integer> removeKDigits(int[] A, int k) {
         // TODO: your code here
         // For now, return a List that's correct size, but contains only 0s
-        List<Integer> l = new LinkedList<>();
-        for (int i = 0; i < A.length - k; i++) l.add(0);
-        return l;
+        LinkedList<Integer> l = new LinkedList<>();
+       // LinkedList<Integer> m = new LinkedList<>();
+        int count =0;
+        for (int i = 0; i < A.length ; i++) {
+            int current = A[i];
+            while (!l.isEmpty() && current < l.peekLast() && count < k) {
+                l.removeLast();
+                count++;
+
+            }
+            if (l.size() < A.length - k) {
+                l.addLast(current);
+            }
+        }
+            return l;
+
     }
 
+
     public static boolean isPalindrome(Node n) {
-        // TODO: your code here
-        return false;
+
+        Node current = n;
+        int size = 0;
+        while(current != null){
+            current = current.next;
+            size++;
+        }
+        if(size < 2){return true;}
+
+        Node next = null;
+        Node prev = null;
+        current = n;
+        int mid = (size/2 + size%2);
+        int count = 0;
+        while(current != null){
+            if(count >= mid){
+                //System.out.println("hit");
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+                count++;
+            }else {
+                //System.out.println("pop");
+                current = current.next;
+                count++;
+            }
+        }
+        Node fount = n;
+        for(int i = 0; i < size/2; i++){
+            if(fount.val!=prev.val){
+                return false;
+            }
+            //stem.out.print();
+            //stem.out.println();
+            fount= fount.next;
+            prev = prev.next;
+        }
+        return true;
     }
 
     public static String infixToPostfix(String s) {
         // TODO
-        return null;
+        LinkedList<Character> l = new LinkedList<>();
+        String sub = "";
+        for(int i=0; i<s.length();i++){
+            if(Character.isDigit(s.charAt(i))){
+                sub = sub+s.charAt(i)+" ";
+            }
+            if(s.charAt(i) == '+' || s.charAt(i) == '*'){
+                l.addFirst(s.charAt(i));}
+            if(s.charAt(i) ==')'){
+                sub = sub+l.getFirst()+' ';
+                l.removeFirst();
+            }
+        }
+        //System.out.println(sub);
+
+        return sub.substring(0,sub.length() - 1);
     }
 
 }
---
+++ datastructures/day04/src/MyHashMap.java
@@ -0,0 +1,244 @@
+import java.util.*;
+
+public class MyHashMap<K, V> implements Map<K, V> {
+
+    // average number of entries per bucket before we grow the map
+    private static final double ALPHA = 1.0;
+    // average number of entries per bucket before we shrink the map
+    private static final double BETA = .25;
+
+    // resizing factor: (new size) = (old size) * (resize factor)
+    private static final double SHRINK_FACTOR = 0.5, GROWTH_FACTOR = 2.0;
+
+    private static final int MIN_BUCKETS = 16;
+
+    // array of buckets
+    protected LinkedList<Entry>[] buckets;
+    private int size = 0;
+
+    public MyHashMap() {
+        initBuckets(MIN_BUCKETS);
+    }
+
+    public class Entry implements Map.Entry<K, V> {
+        private K key;
+        private V value;
+
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V newValue) {
+            value = newValue;
+            return value;
+        }
+    }
+
+    /**
+     * given a key, return the bucket where the `K, V` pair would be stored if it were in the map.
+     */
+    private LinkedList<Entry> chooseBucket(Object key) {
+        // TODO
+        // hint: use key.hashCode() to calculate the key's hashCode using its built in hash function
+        // then use % to choose which bucket to return.
+        //System.out.print(buckets);
+
+        int idx = key.hashCode() % buckets.length;
+        return buckets[idx];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * return true if key is in map
+     */
+    @Override
+    public boolean containsKey(Object key) {
+        // TODO
+        LinkedList<Entry> bucket = chooseBucket(key);
+        for(int i = 0; i < bucket.size(); i++) {
+            if (bucket.peekFirst().key .equals(key)) {
+                return true;
+            }
+            Entry temp = bucket.peekFirst();
+            bucket.removeFirst();
+            bucket.addLast(temp);
+        }
+        return false;
+    }
+
+    /**
+     * return true if value is in map
+     */
+    @Override
+    public boolean containsValue(Object value) {
+        // TODO
+        for(int i = 0; i < buckets.length; i++){
+            for(int j = 0; j < buckets[i].size(); j++) {
+                if (buckets[i].get(j).value == value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        // TODO
+        LinkedList<Entry> bucket = chooseBucket(key);
+        for(int i = 0; i < bucket.size(); i++) {
+            if (bucket.peekFirst().key .equals(key)) {
+                return bucket.peekFirst().value;
+            }
+            Entry temp = bucket.peekFirst();
+            bucket.removeFirst();
+            bucket.addLast(temp);
+        }
+        return null;
+    }
+
+    /**
+     * add a new key-value pair to the map. Grow if needed, according to `ALPHA`.
+     * @return the value associated with the key if it was previously in the map, otherwise null.
+     */
+    @Override
+    public V put(K key, V value) {
+        // TODO: Complete this method
+        // hint: use chooseBucket() to determine which bucket to place the pair in
+        // hint: use rehash() to appropriately grow the hashmap if
+        if(size  > buckets.length * ALPHA  ){rehash(GROWTH_FACTOR);}
+        V val = null;
+
+        if (containsKey(key)){
+             val = remove(key);
+        }
+        LinkedList<Entry> bucket = chooseBucket(key);
+        Entry temp = new Entry(key , value);
+        bucket.add(temp);
+        size++;
+        return val ;
+    }
+
+    /**
+     * Remove the key-value pair associated with the given key. Shrink if needed, according to `BETA`.
+     * Make sure the number of buckets doesn't go below `MIN_BUCKETS`. Do nothing if the key is not in the map.
+     * @return the value associated with the key if it was in the map, otherwise null.
+     */
+    @Override
+    public V remove(Object key) {
+        // TODO
+        // hint: use chooseBucket() to determine which bucket the key would be
+        // hint: use rehash() to appropriately grow the hashmap if needed
+        if(size < buckets.length * BETA ){rehash(SHRINK_FACTOR);}
+        LinkedList<Entry> bucket = chooseBucket(key);
+        for(int i = 0; i < bucket.size(); i++) {
+            if (bucket.peekFirst().key .equals(key)) {
+                V val = bucket.peekFirst().value;
+                bucket.removeFirst();
+                size--;
+                return val;
+            }
+            Entry temp = bucket.peekFirst();
+            bucket.removeFirst();
+            bucket.addLast(temp);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        for (Map.Entry<? extends K, ? extends V> entry : m.entrySet()) {
+            put(entry.getKey(), entry.getValue());
+        }
+    }
+
+    /**
+     * Changes the number of buckets and rehashes the existing entries.
+     * If growthFactor is 2, the number of buckets doubles. If it is 0.25,
+     * the number of buckets is divided by 4.
+     */
+    private void rehash(double growthFactor) {
+        // TODO
+        // hint: once you have removed all values from the buckets, use put(k, v) to add them back in the correct place
+        LinkedList<Entry> temp = new LinkedList<>();
+        for(int i = 0; i < buckets.length; i++){
+            for(int j = 0; j < buckets[i].size(); j++) {
+                temp.add(buckets[i].get(j));
+            }
+        }
+
+        int newsize = (int) (buckets.length * growthFactor);
+        if (newsize < 16){newsize = 16;}
+        initBuckets(newsize);
+        size = 0;
+        while(!temp.isEmpty()){
+            put(temp.getFirst().key,temp.getFirst().value);
+            temp.removeFirst();
+        }
+
+
+
+
+    }
+
+    private void initBuckets(int size) {
+        buckets = new LinkedList[size];
+        for (int i = 0; i < size; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    public void clear() {
+        initBuckets(buckets.length);
+        size = 0;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        Set<K> keys = new HashSet<>();
+        for (Map.Entry<K, V> e : entrySet()) {
+            keys.add(e.getKey());
+        }
+        return keys;
+    }
+
+    @Override
+    public Collection<V> values() {
+        Collection<V> values = new ArrayList<>();
+        for (Map.Entry<K, V> e : entrySet()) {
+            values.add(e.getValue());
+        }
+        return values;
+    }
+
+    @Override
+    public Set<Map.Entry<K, V>> entrySet() {
+        Set<Map.Entry<K, V>> set = new HashSet<>();
+        for (LinkedList<Entry> map : buckets) {
+            set.addAll(map);
+        }
+        return set;
+    }
+}
